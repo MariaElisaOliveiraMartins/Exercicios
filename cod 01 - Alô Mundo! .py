ac76cdcca43e822b51cbac6e30d82ad604460d60
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-01-02 18:24:06 -0200

    Função sem parâmetros.

---
+++ cod 01 - Alô Mundo! .py
@@ -0,0 +1,9 @@
+''' Exemplo de função s/ parametros'''
+
+def alo_mundo():
+    return '\n   Alô Mundo  !!! \n'
+
+print('\nExemplo de função s/ parametros' )
+print(alo_mundo())
+print(type(alo_mundo()))
+print ('\n')
