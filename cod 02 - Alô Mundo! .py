fbe6c8dd99e730d69189cc6280154e2033cae853
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-01-02 17:44:00 -0200

    Primeiro teste de função.

--- cod 02 - Alô Mundo! .py
+++
@@ -1,8 +0,0 @@
-''' Exemplo de função c/ parametro'''
-
-def alo_mundo(parametro):
-    return f'=====> {parametro} <=====\n'
-
-print('\nExemplo de função c/ parametros\n')
-print(alo_mundo('Alô Mundo!'))
-print(type(alo_mundo))
--- cod 13 - Teste de Commit GitHub.py
+++
@@ -1,8 +0,0 @@
-''' Esse código tem a simples finalidade de treinar o uso do Git e do GitHub'''
-
-def func_nao_faz_nada ():
-    return('Não fiz nada!')
-
-testa_nao_faz_nada = func_nao_faz_nada()
-
-print(testa_nao_faz_nada)
