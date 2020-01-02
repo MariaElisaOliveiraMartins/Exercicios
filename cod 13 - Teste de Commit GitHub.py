67a3a63bec325e3082ba05c403e717aab7c76e03
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-01-02 18:47:57 -0200

    Código para treinamento do uso do GitHub.

---
+++ cod 13 - Teste de Commit GitHub.py
@@ -0,0 +1,11 @@
+''' Esse código tem a simples finalidade de treinar o uso do Git e do GitHub'''
+
+def func_teste_commit (param):
+
+    return('===>', len(param), '<===')
+
+var_teste = func_teste_commit('abcdefghijklm')
+print('\n',var_teste)
+
+var_teste = func_teste_commit('hijklmnopqrstuvxz')
+print('\n',var_teste)
