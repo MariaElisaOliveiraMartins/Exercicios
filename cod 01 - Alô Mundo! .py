80ae6bd6301532ab35c251af8ff548b379214213
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-01-29 21:32:36 -0200

    Alteração na string

--- cod 01 - Alô Mundo! .py
+++ cod 01 - Alô Mundo! .py
@@ -1,7 +1,7 @@
 ''' Exemplo de função s/ parametros'''
 
 def alo_mundo():
-    return '\n  Alô Mundo  !!! \n'
+    return '\n  Hello World !!! \n'
 
 print('\nExemplo de função s/ parametros' )
 print(alo_mundo())
