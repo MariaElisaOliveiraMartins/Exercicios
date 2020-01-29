16fd1db01ddfdf7ce1716009f6653d16384ed8e0
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-01-29 11:32:05 -0200

    reovido o teste de integração pycharm x girhub

--- cod teste.py
+++
@@ -1,9 +0,0 @@
-''' Exemplo de função s/ parametros'''
-
-def alo_mundo():
-    return '\n  Alô Mundo  !!!!!!! \n'
-
-print('\nExemplo de função s/ parametros' )
-print(alo_mundo())
-print(type(alo_mundo()))
-print ('\n')
